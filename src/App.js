7ddf74d7c35b3936598ded99f0b48109ec17019c
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-10-17 19:46:58 -0700

    add axios call to API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
+
 import "./App.css";
 
 function App() {
+
+  const [pictureURL, setPictureURL] = useState(null);
+
+  useEffect(() => {
+
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then( response => {
+
+        console.log("Picture found", response);
+
+        setPictureURL(response);
+
+      })
+      .catch( error => {
+        console.log("Error:", error);
+      })
+
+  }, [])
+
   return (
     <div className="App">
       <p>
-        
+        { pictureURL ? <img src={pictureURL} /> : "Loading..." }
       </p>
     </div>
   );
